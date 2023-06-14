55d399d0fa1419e0906f494fe4553b22b104ca3c
kanggicheon <snoopy1516@naver.com>
2023-06-14 11:55:47 +0900

    react-prettier
    prettier 확장 프로그램 사용해보기

---
+++ App-prettier.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  const name = "react";
+  return (
+    <div>
+      <div className="react">{name}</div>
+      <h1>들여쓰기가 이상한</h1>
+      <h2>코드</h2>
+      <p>입니다.</p>
+    </div>
+  );
+}
+// ctrl + shift + p -> format(문서 서식) 입력
+export default App;
