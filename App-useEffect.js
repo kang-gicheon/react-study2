045b10c42e772fa94976b100f8543bc3c63f28a2
kanggicheon <snoopy1516@naver.com>
2023-06-19 12:20:15 +0900

    UseEffect
    hook - useEffect 기능 학습

---
+++ App-useEffect.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import Info from "./info";
+
+const App = () => {
+  return <Info />;
+};
+
+export default App;
---
+++ info-useEffect.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  useEffect(() => {
+    console.log("렌더링이 완료되었습니다!");
+    console.log({
+      name,
+      nickname,
+    });
+  });
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Info;
