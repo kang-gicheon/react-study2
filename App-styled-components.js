6b6522fea27f9dfa237b2f1f93dd74fbef5bb7af
kanggicheon <snoopy1516@naver.com>
2023-06-22 17:32:09 +0900

    styled-components
    styled-compoonents실습, 사용 시
    yarn add styled-components로 설치 필요

---
+++ App-styled-components.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import StyledComponent from "./StyledComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <StyledComponent />
+      </div>
+    );
+  }
+}
+export default App;
