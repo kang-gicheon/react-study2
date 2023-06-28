e3733a19a621bca11a78870d91928e926f767b42
kanggicheon <snoopy1516@naver.com>
2023-06-28 17:54:02 +0900

    Select Color component
    색상 선택 컴포넌트 만들기, app.js(프로젝트 적용)

---
+++ Context API/SelectColors.js
@@ -0,0 +1,26 @@
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+const SelectColors = () => {
+  return (
+    <div>
+      <h2>색상 선택</h2>
+
+      <div style={{ display: "flex" }}>
+        {colors.map((color) => (
+          <div
+            key={color}
+            style={{
+              background: color,
+              width: "24px",
+              height: "24px",
+              cursor: "pointer",
+            }}
+          />
+        ))}
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default SelectColors;
