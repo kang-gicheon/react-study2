fffb4fbb7c0d802a45b1757fd7a2952b04bcdeee
kanggicheon <snoopy1516@naver.com>
2023-06-28 16:36:14 +0900

    Consumer
    Component폴더 생성, ColorBox 생성

---
+++ Context API/ColorBox.js
@@ -0,0 +1,17 @@
+import ColorContext from "../contexts/color";
+
+const ColorBox = () => {
+  return (
+    <ColorContext.Consumer>
+      {(value) => (
+        <div
+          style={{
+            width: "64px",
+            height: "64px",
+            background: value.color,
+          }}
+        />
+      )}
+    </ColorContext.Consumer>
+  );
+};
