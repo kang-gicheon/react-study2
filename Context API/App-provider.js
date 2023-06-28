3bd0f561b76f070ee0556e4f1a87ffc42c8da166
kanggicheon <snoopy1516@naver.com>
2023-06-28 17:15:49 +0900

    Provider-app.js
    provider를 이용한 context value 변경

---
+++ Context API/App-provider.js
@@ -0,0 +1,14 @@
+import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
+
+const App = () => {
+  return (
+    <ColorContext.Provider value={{ color: "red" }}>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
+  );
+};
+
+export default App;
